2dc2b5927878f206fb0439374a2b0197549bc2bc
Arati Acharya <arati.acharya.2023@student.lhschools.org>
2020-03-02 12:10:44 -0500

    intelliji commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import java.text.MessageFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Phase 1
+
+        String specialPerson; //declaring a variable
+        String place;
+        String relation;
+        String partner;
+        String fate;
+        String expire;
+        String sign;
+        String concern;
+        String shining;
+        String make;
+        String cause;
+        String time;
+        String coast;
+        String everybody;
+        String trust;
+        String few;
+        String groupOfPeople;
+        String planet;
+        String living;
+        String find;
+        Scanner rabbta;
+
+        //phase 2
+        //initializing
+        rabbta = new Scanner(System.in);
+
+        System.out.println("song");
+        System.out.println("Raabta");
+
+
+
+        //phase 3
+        System.out.println("They say that in this, +"place);
+        System.out.println("God has  "+ make +" "+ specialPerson + " for "+ groupOfPeople);
+        System.out.println("I believe that I met you due to God's "+sign);
+        System.out.println("He has  "+ make + " me for "+ specialPerson + " like you");
+        System.out.println("They say that in this " + place );
+        System.out.println("God has "+ make + ""+ specialPerson + " for "+ groupOfPeople);
+        System.out.println("I believe that I met you due to God's "+ sign );
+        System.out.println("He has  "+ make + " me for "+ specialPerson + " like you");
+        System.out.println("I do have  "+ few +" "+ relation +" with you");
+        System.out.println("I do have  "+ few +"  "+ relation +" with you");
+        System.out.println("I don't know how to find that out");
+        System.out.println("I do have "+ few + "  "+ relation + " with you");
+        System.out.println("You're my "+ partner);
+        System.out.println("Hence there's nothing to "+ concern);
+        System.out.println("You're my "+ cause + "  to live and "+ expire);
+
+        System.out.println("They say that in this " + place);
+        System.out.println("God has "+ make + ""+ specialPerson +" for "+ groupOfPeople);
+
+        System.out.println("It did a favour on me as it departed");
+        System.out.println("A "+ time + " has filled me up");
+        System.out.println("It's like attaining a " + shining+" " + planet+" when I see you");
+        System.out.println("It's like the boats of my  "+ fate + " have "+ find + " the"+ coast + "");
+
+        System.out.println("The kind of  "+ living + " that I've yearned for years");
+        System.out.println("I simply pray for that in your company");
+        System.out.println("I " + trust +" that I met you due to God's "+ sign );
+        System.out.println("He has  "+ make + " me for "+ specialPerson + " like you");
+        System.out.println("I do have  "+ few + "  "+ relation + " with you");
+        System.out.println("I do have  "+ few + " "+ relation + " with you");
+        System.out.println("You're my "+ partner );
+        System.out.println("Hence there's nothing to"+ partner );
+        System.out.println("You're my "+ cause +" to live and "+ expire);
+
+        System.out.println("They say that in this" + place);
+        System.out.println("God has  "+ make + " "+ specialPerson + " for "+ groupOfPeople);
+
+    }
+}
